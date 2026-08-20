fa2e06fe259fd1ed05556618af8fdc6b4d04689f
Ruben Gustaf Ingemar Rick <ruben.rick@outlook.com>
2026-08-20 19:30:01 +0700

    claude-code checkpoint 2026-08-20 19:30:01

--- app/src/components/BlankCards.jsx
+++ app/src/components/BlankCards.jsx
@@ -1,9 +1,9 @@
 export default function BlankCards() {
 	return (
 		<div style={{ display: "flex", flexWrap: "wrap", gap: "16px", paddingLeft: "16px", paddingRight: "16px" }}>
-			<div className="card" style={{ flex: "1 1 200px", height: "100px" }} />
-			<div className="card" style={{ flex: "1 1 200px", height: "100px" }} />
-			<div className="card" style={{ flex: "1 1 200px", height: "100px" }} />
+			<div className="card" style={{ flex: "0 0 200px", width: "200px", height: "100px" }} />
+			<div className="card" style={{ flex: "0 0 200px", width: "200px", height: "100px" }} />
+			<div className="card" style={{ flex: "0 0 200px", width: "200px", height: "100px" }} />
 		</div>
 	);
 }

b579ff682c638094dcfe21e5c0bd1cda84dc55a3
Ruben Gustaf Ingemar Rick <ruben.rick@outlook.com>
2026-08-20 17:43:06 +0700

    claude-code checkpoint 2026-08-20 17:43:06

--- app/src/pages/Results.jsx
+++ app/src/pages/Results.jsx
@@ -7,13 +7,11 @@ export default function Results() {
 			<br />
 			<div className="row">
 				<div className="col-md">
-					<div className="card card-body">
-						<iframe
-							src="/extplorer/index.html"
-							title="Results file explorer"
-							style={{ width: "100%", height: "600px", border: "none" }}
-						/>
-					</div>
+					<iframe
+						src="/extplorer/index.html"
+						title="Results file explorer"
+						style={{ width: "100%", height: "600px", border: "none", display: "block" }}
+					/>
 				</div>
 			</div>
 		</>

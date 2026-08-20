abda7278f46339e5792d5aabe0a0eade11c99e4e
Ruben Gustaf Ingemar Rick <ruben.rick@outlook.com>
2026-08-20 21:07:47 +0700

    claude-code checkpoint 2026-08-20 21:07:47

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -4,17 +4,20 @@ import SupportChat from "./SupportChat";
 
 export default function Navbar({ variant }) {
 	return (
-		<nav className="navbar navbar-dark dark">
-			<img className="mr-4" src="/img/navbar-logo.png" height="37" alt="Logo" />
-			<ul className="navbar-nav">
-				<li className="nav-item">
-					<Link className="nav-link" to="/user">Home</Link>
-				</li>
-			</ul>
-			<div className="ml-auto">
-				<UserButton afterSignOutUrl="/" />
-			</div>
-			<SupportChat />
-		</nav>
+		<>
+			<nav className="navbar navbar-dark dark">
+				<img className="mr-4" src="/img/navbar-logo.png" height="37" alt="Logo" />
+				<ul className="navbar-nav">
+					<li className="nav-item">
+						<Link className="nav-link" to="/user">Home</Link>
+					</li>
+				</ul>
+				<div className="ml-auto">
+					<UserButton afterSignOutUrl="/" />
+				</div>
+				<SupportChat />
+			</nav>
+			<div style={{ height: "60px" }} />
+		</>
 	);
 }

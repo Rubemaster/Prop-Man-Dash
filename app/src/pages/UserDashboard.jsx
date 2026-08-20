7ef32b45d9550ca45f87b7c323fd6261f92c037c
Ruben Gustaf Ingemar Rick <ruben.rick@outlook.com>
2026-08-20 20:23:18 +0700

    claude-code checkpoint 2026-08-20 20:23:18

--- app/src/pages/UserDashboard.jsx
+++ app/src/pages/UserDashboard.jsx
@@ -30,7 +30,7 @@ export default function UserDashboard() {
 				<br />
 				<StatusCards total={propertyCount} delivered={inspectionCount} />
 				<br />
-				<StatusCards total={propertyCount} delivered={inspectionCount} hidePropertiesContent propertiesRepeat={4} showOtherCards={false} cardHeight={50} />
+				<StatusCards total={propertyCount} delivered={inspectionCount} hidePropertiesContent propertiesRepeat={4} showOtherCards={false} cardHeight={30} />
 				<br />
 				<div className="row">
 					<div className="col">

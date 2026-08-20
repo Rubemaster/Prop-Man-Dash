bd9e3ab87890edffa5406777e47a399e9f23d92e
Ruben Gustaf Ingemar Rick <ruben.rick@outlook.com>
2026-08-20 18:54:21 +0700

    claude-code checkpoint 2026-08-20 18:54:21

--- app/src/pages/UserDashboard.jsx
+++ app/src/pages/UserDashboard.jsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 import { useUser } from "@clerk/clerk-react";
 import Navbar from "../components/Navbar";
 import StatusCards from "../components/StatusCards";
-import DataTable from "../components/DataTable";
-import { TABLE_COLUMNS, SAMPLE_ROW } from "../tableColumns";
 
 export default function UserDashboard() {
 	const { user } = useUser();
@@ -26,13 +24,6 @@ export default function UserDashboard() {
 			<Navbar variant="customer" />
 			<br />
 			<StatusCards total={propertyCount} delivered={inspectionCount} />
-			<div className="row">
-				<div className="col-md">
-					<div className="card card-body">
-						<DataTable columns={TABLE_COLUMNS} rows={[SAMPLE_ROW]} />
-					</div>
-				</div>
-			</div>
 			<br />
 			<div className="row">
 				<div className="col">

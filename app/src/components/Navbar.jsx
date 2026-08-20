553317df59a66256a22ec33c2c075f13fd8bbe31
Ruben Gustaf Ingemar Rick <ruben.rick@outlook.com>
2026-08-20 18:44:49 +0700

    claude-code checkpoint 2026-08-20 18:44:49

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -4,26 +4,13 @@ import SupportChat from "./SupportChat";
 
 export default function Navbar({ variant }) {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark dark">
+		<nav className="navbar navbar-dark dark">
 			<img className="mr-4" src="/img/logoCRM.png" width="41" height="37" alt="CRM logo" />
-			<button
-				className="navbar-toggler"
-				type="button"
-				data-toggle="collapse"
-				data-target="#navbarNav"
-				aria-controls="navbarNav"
-				aria-expanded="false"
-				aria-label="Toggle navigation"
-			>
-				<span className="navbar-toggler-icon"></span>
-			</button>
-			<div className="collapse navbar-collapse" id="navbarNav">
-				<ul className="navbar-nav">
-					<li className="nav-item">
-						<Link className="nav-link" to="/user">Home</Link>
-					</li>
-				</ul>
-			</div>
+			<ul className="navbar-nav">
+				<li className="nav-item">
+					<Link className="nav-link" to="/user">Home</Link>
+				</li>
+			</ul>
 			<div className="ml-auto">
 				<UserButton afterSignOutUrl="/" />
 			</div>
